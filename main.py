1d7837b664ba7a20d4f60ec3303f163c5fdee244
Mitchell Busby <mitchbusby@gmail.com>
2020-01-06 19:58:27 +1100

    it all works

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from geojson import Feature, Polygon, load
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 
-ax = plt.axes(projection=ccrs.PlateCarree())
 # ax.stock_img()
 
-f = open('suburb-2-nsw.geojson')
+f = open('input/mel-teams.geojson')
 
 coords = load(f)
 
-selected_suburb = coords["features"][0]
-print(selected_suburb)
-
 def make_aggregate_lat_lon(suburb):
   individual_coords = suburb['geometry']['coordinates']
-  # print(individual_coords)
+
   # geojson goes long/lat
   agg_lon = list(map(lambda x: x[0], individual_coords[0]))
   agg_lat = list(map(lambda x: x[1], individual_coords[0]))
@@ -22,16 +18,20 @@ def make_aggregate_lat_lon(suburb):
   return agg_lon, agg_lat
 
 def make_suburb_map(suburb):
+  ax = plt.axes(projection=ccrs.PlateCarree())
+
   agg_lon, agg_lat = make_aggregate_lat_lon(suburb)
 
   plt.plot(agg_lon, agg_lat,
           color='blue', linewidth=2,
           transform=ccrs.Geodetic(),
           )
+  save_map(suburb)
+  plt.clf()
 
-  plt.cla()
+def save_map(suburb):
+  suburb_name = suburb['properties']['name']
+  plt.savefig(f'output/{suburb_name}.png')
 
-for suburb in coords["features"][:100]:
+for suburb in coords[:100]:
   make_suburb_map(suburb)
-
-plt.show()
