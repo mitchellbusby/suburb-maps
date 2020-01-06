f3633affa0dea8b86497ae6fd5eeb780146b3469
Mitchell Busby <mitchbusby@gmail.com>
2020-01-06 20:45:36 +1100

    A wip

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from geojson import Feature, Polygon, load
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-
-# ax.stock_img()
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import numpy as np
 
 f = open('input/syd-teams.geojson')
 
@@ -19,6 +20,11 @@ def make_aggregate_lat_lon(suburb):
 
   return agg_lon, agg_lat
 
+def make_numpy_array_from_vertices(suburb):
+  individual_coords = suburb['geometry']['coordinates'][0]
+
+  return np.array(individual_coords)
+
 def make_suburb_map(suburb):
   ax = plt.axes(projection=ccrs.PlateCarree())
 
@@ -26,14 +32,22 @@ def make_suburb_map(suburb):
   plt.plot(agg_lon, agg_lat,
           color='#2A80B9',
           linewidth=2,
-          transform=ccrs.Geodetic(),
+          transform=ccrs.Geodetic()
           )
+
   """
-  plt.fill_between(agg_lon, agg_lat,
-          facecolor='#2A80B9',
-          transform=ccrs.Geodetic(),
+  I started doing this but it's not working
+  and I don't understand why
+  print(np.column_stack([agg_lat, agg_lon]))
+  poly = Polygon(np.column_stack([agg_lat, agg_lon]),
+    color="#2A80B9",
+    # fill=True,
+    transform=ccrs.Geodetic()
   )
+
+  ax.add_patch(poly)
   """
+
   save_map(suburb)
   plt.clf()
 
